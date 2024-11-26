a5f85de8a41fd4f8240e428fc9c3d6dfbf1f36d6
Zinc-zn <riskiafifudin05@gmail.com>
2024-11-27 01:08:04 +0700

    update 1

--- percobaan2.java
+++ percobaan2.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class percobaan2 {
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
-            return 1; 
+            return 1;
         } else {
-            int hasil = x * hitungPangkat(x, y - 1);
-
             if (y > 1) {
                 System.out.print(x + " x ");
             } else {
                 System.out.print(x);
             }
-            return hasil;
+            return x * hitungPangkat(x, y - 1);
         }
     }
 
